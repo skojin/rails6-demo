94647ed6fed9c9cb41305e509047255f9b2de598
Sergey Kojin <sergey.kojin@gmail.com>
2020-01-29 16:35:58 +0300

    make test:system headless

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,16 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+ driven_by(
+      :selenium,
+      using: :headless_chrome,
+      screen_size: [1400, 1400],
+      options: {
+        desired_capabilities: {
+          chromeOptions: {
+            args: %w[disable-gpu no-sandbox],
+          },
+        },
+      }
+    )
 end
